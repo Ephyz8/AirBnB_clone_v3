0e6dd0fe4d176c4e43c89effa88f53602c81b2ae
Ephyz8 <ziwoyaephraim@gmail.com>
2024-05-01 20:43:34 +0200

    edit

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,6 +2,7 @@
 """
 Script for views
 """
+from flask import Blueprint
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.amenities import *
@@ -10,7 +11,6 @@ from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.users import *
 from api.v1.views.places_amenities import *
-from flask import Blueprint
 
 
 app_views = Blueprint('/api/v1', __name__, url_prefix="/api/v1")
