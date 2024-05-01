68ea0941b13fcb2cf5fd8406fd398553ed6df1c0
Ephyz8 <ziwoyaephraim@gmail.com>
2024-05-01 18:34:13 +0200

    index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,13 +1,44 @@
-#!usr/bin/python3
-"""Creates Flask app; app_views."""
+#!/usr/bin/python3
+"""
+Script for index
+"""
+
 from flask import jsonify
 from api.v1.views import app_views
 
+from models import storage
 
-@app_views.route('/status')
-def api_status():
-  """
-  Returns JSON response.
-  """
-  resp = {'status': "OK"}
-  return jsonify(resp)
+
+@app_views.route("/status", methods=['GET'], strict_slashes=False)
+def status():
+    """
+    status route:
+    return: json response
+    """
+    data = {"status": "OK"}
+
+    respns = jsonify(data)
+    respns.status_code = 200
+
+    return respns
+
+
+@app_views.route("/stats", methods=['GET'], strict_slashes=False)
+def stats():
+    """
+    stats of all objs route
+    :return: json of all objs
+    """
+    data = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
+    }
+
+    respns = jsonify(data)
+    respns.status_code = 200
+
+    return respns
