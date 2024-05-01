2fb4055fca3b25d1c9c266d72c393505a51d039e
Ephyz8 <ziwoyaephraim@gmail.com>
2024-05-01 18:04:30 +0200

    edit

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,10 +2,11 @@
 """
 Creates Flask app, and register app_views layout to Flask instance app.
 """
+from os import getenv
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
-from os import getenv
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -13,5 +14,5 @@ app.register_blueprint(app_views)
 
 if __name__ == '__main__':
   HOST = getenv('HBNB_API_HOST', '0.0.0.0')
-  PORT = int(getenv('HBNB_API_PORT', 5000))
+  PORT = getenv('HBNB_API_PORT')
   app.run(host=HOST, port=PORT, threaded=True)
